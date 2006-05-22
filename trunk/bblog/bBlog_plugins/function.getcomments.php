5a576e66b32d3e42c4e8b53b6a6d27b53940c08e
kpower <kpower@1602e9e2-f712-0410-8437-affbadaecd9e>
2006-05-22 16:10:38 +0000

    Added ADODB library update function.getcomments to work with new commenthandler class updated dmig/post.html for new comment handling

--- trunk/bblog/bBlog_plugins/function.getcomments.php
+++ trunk/bblog/bBlog_plugins/function.getcomments.php
@@ -20,60 +20,61 @@
 ** Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 function identify_function_getcomments () {
-$help = '
-<p>Provides threaded comments. See the default templates for usage examples.';
+    $help = '<p>Provides threaded comments. See the default templates for usage examples.';
 
-  return array (
-    'name'           =>'getcomments',
-    'type'             =>'function',
-    'nicename'     =>'Get Comments',
-    'description'   =>'Gets Comments and trackbacks for a post and threads them',
-    'authors'        =>'Eaden McKee <eaden@eadz.co.nz>',
-    'licence'         =>'GPL',
-    'help'   => $help
-  );
+    return array (
+        'name'           =>'getcomments',
+        'type'             =>'function',
+        'nicename'     =>'Get Comments',
+        'description'   =>'Gets Comments and trackbacks for a post and threads them',
+        'authors'        =>'Eaden McKee <eaden@eadz.co.nz>',
+        'licence'         =>'GPL',
+        'help'   => $help
+    );
 }
 function smarty_function_getcomments ($params, &$bBlog) {
 	$assign="comments";
 	$postid=$bBlog->show_post;
 	$replyto = $_REQUEST['replyto'];
-	extract($params);
-
-        // first, assign the hidden fields
-        $commentformhiddenfields = '<input type="hidden" name="do" value="submitcomment" />';
-        $commentformhiddenfields .='<input type="hidden" name="comment_postid" value="'.$postid.'" />';
-        if(is_numeric($replyto)) {
-              $commentformhiddenfields .= '<a name="commentform"></a><input type="hidden" name="replytos" value="'.$replyto.'" />';
+    $ch = $bBlog->get_comment_handler($_REQUEST['postid']);
+    prep_form(&$bBlog, $_REQUEST['postid'], $_REQUEST['replyto']);
+    // are we posting a comment ?
+    if($_POST['do'] == 'submitcomment' && is_numeric($_POST['comment_postid'])) { // we are indeed!
+        if(is_numeric($_POST['replytos'])){
+            $rt = $_POST['replytos'];
         }
-        $bBlog->assign("commentformhiddenfields",$commentformhiddenfields);
-        $bBlog->assign("commentformaction",$bBlog->_get_entry_permalink($postid));
-        // are we posting a comment ?
-        if($_POST['do'] == 'submitcomment' && is_numeric($_POST['comment_postid'])) { // we are indeed!
-            if(is_numeric($_POST['replytos'])) $rt = $_POST['replytos'];
-            else $rt =  false;
-
-            $bBlog->new_comment($_POST['comment_postid'],$rt);
+        else{
+            $rt =  false;
         }
-                   
-        // get the comments.
+        $ch->new_comment($bBlog->_ph->get_post($_POST['comment_postid'], false, true),$rt, $_POST);
+    }
+               
+    // get the comments.
+    
+    /* start loop and get posts*/
+    
+    $rt = false;
+    if(is_numeric($_GET['replyto'])) {
+        $rt = $_GET['replyto'];
+        $cs = $ch->get_comment($_REQUEST['postid'],$rt);
+    } else {
+        $cs = $ch->get_comments();
         
-        /* start loop and get posts*/
-        $rt = false;
-        if(is_numeric($_GET['replyto'])) {
-            $rt = $_GET['replyto'];
-            $cs = $bBlog->get_comment($postid,$rt);
-        } else {
-            $cs = $bBlog->get_comments($postid,FALSE);
-        }
-        
+    }
+    /* assign loop variable */
+    $bBlog->assign($assign, $cs);
+}
 
-
-          /* assign loop variable */
-        $bBlog->assign($assign, $cs);
-
-
-
-
+function prep_form(&$bBlog, $postid, $replyto){
+    //first, assign the hidden fields
+    $commentformhiddenfields = '<input type="hidden" name="do" value="submitcomment" />';
+    $commentformhiddenfields .='<input type="hidden" name="comment_postid" value="'.$postid.'" />';
+    if(is_numeric($replyto)) {
+          $commentformhiddenfields .= '<a name="commentform"></a><input type="hidden" name="replytos" value="'.$replyto.'" />';
+    }
+    $bBlog->assign("commentformhiddenfields",$commentformhiddenfields);
+    $bBlog->assign("commentformaction",$bBlog->_get_entry_permalink($postid));
+    
 }
 
 ?>
